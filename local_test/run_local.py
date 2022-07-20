046ac28def62d85386489e7fb0e53813e9043a4d
Abu Desai <abhyuday.desai@gmail.com>
2022-07-20 08:58:51 -0700

    regressor

--- local_test/run_local.py
+++ local_test/run_local.py
@@ -11,9 +11,8 @@ import algorithm.model_trainer as model_trainer
 import algorithm.model_server as model_server
 import algorithm.model_tuner as model_tuner
 import algorithm.preprocessing.pipeline as pipeline
-import algorithm.model.adaboost as adaboost
+import algorithm.model.regressor as regressor
 
-model_name= "AdaBoostReg"
 inputs_path = "./ml_vol/inputs/"
 
 data_schema_path = os.path.join(inputs_path, "data_config")
@@ -43,7 +42,7 @@ from requirements.txt file, and then use that virtual env to do your testing.
 This isnt foolproof. You can still have host os or python version-related issues, so beware.
 '''
 
-model_name= adaboost.MODEL_NAME
+model_name= regressor.MODEL_NAME
 
 
 def create_ml_vol():
@@ -110,7 +109,7 @@ def train_and_save_algo():
     # Save the processing pipeline
     pipeline.save_preprocessor(preprocessor, model_artifacts_path)
     # Save the model
-    adaboost.save_model(model, model_artifacts_path)
+    regressor.save_model(model, model_artifacts_path)
     print("done with training")
 
 def load_and_test_algo():
@@ -196,10 +195,10 @@ if __name__ == "__main__":
     
     num_hpt_trials = 20
     run_hpt_list = [False, True]
-    # run_hpt_list = [False]
+    run_hpt_list = [False]
     
     datasets = ["abalone", "auto_prices", "computer_activity", "heart_disease", "white_wine", "ailerons"]
-    # datasets = ["heart_disease"]
+    datasets = ["heart_disease"]
     
     for run_hpt in run_hpt_list:
         all_results = []
